28fbe6b42368468f91306fb7da0a32f6c9621388
githsem <53379599+githsem@users.noreply.github.com>
2020-07-22 16:21:57 +0200

    add

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,13 @@
 
+import java.util.Scanner;
+
+
 public class Test {
     public static void main(String[] args) {
+        System.out.println("Beyblade Programina Hosgeldiniz...");
+        System.out.println("Cikis icin q'ya basiniz...");
+        
+        Scanner scanner = new Scanner(System.in); 
         
     }
     
