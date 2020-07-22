146509ab92caeebd61cd0589878efbfd8be36854
githsem <53379599+githsem@users.noreply.github.com>
2020-07-22 16:30:54 +0200

    add

--- src/Test.java
+++ src/Test.java
@@ -8,6 +8,29 @@ public class Test {
         System.out.println("Cikis icin q'ya basiniz...");
         
         Scanner scanner = new Scanner(System.in); 
+        while(true){
+            System.out.println("Hangi Beyblade' i Uretmek Istiyorsunuz?");
+            String islem = scanner.nextLine();
+            if (islem.equals("q")){
+                System.out.println("Programdan Cikiliyor...");
+                break;
+            }
+            else {
+                BeybladeFabrikasi fabrika = new BeybladeFabrikasi();
+                Beyblade beyblade = fabrika.beybladeUret(islem);
+                if (beyblade == null){
+                    System.out.println("Lutfen Gecerli Bir Beyblade Ismi Girin");
+                }
+                else {
+                    beyblade.bilgileriGoster();
+                    beyblade.saldir();
+                    beyblade.kutsalCanavarOrtayaCikar();
+                }
+            }
+            
+            
+            
+        }
         
     }
     
